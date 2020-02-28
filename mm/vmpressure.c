54093180f38f1c338344857a95636354f64143ef
Sultan Alsawaf <sultan@kerneltoast.com>
2020-02-28 12:38:10 -0800

    mm: vmpressure: Interpret zero scanned pages as 100% pressure
    When no pages are scanned, it usually means no zones were reclaimable
    and nothing could be done. In this case, the reported pressure should be
    100 to elicit help from any listeners. This fixes the vmpressure
    framework not working when memory pressure is very high.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -304,8 +304,13 @@ static void vmpressure_global(gfp_t gfp, unsigned long scanned,
 	unsigned long pressure;
 	unsigned long stall;
 
-	if (!scanned)
-		return;
+	if (scanned) {
+		spin_lock(&vmpr->sr_lock);
+		if (!vmpr->scanned)
+			calculate_vmpressure_win();
+
+		vmpr->scanned += scanned;
+		vmpr->reclaimed += reclaimed;
 
 		if (!current_is_kswapd())
 			vmpr->stall += scanned;
@@ -315,7 +320,7 @@ static void vmpressure_global(gfp_t gfp, unsigned long scanned,
 		reclaimed = vmpr->reclaimed;
 		spin_unlock(&vmpr->sr_lock);
 
-		if (!critical && scanned < calculate_vmpressure_win())
+		if (scanned < vmpressure_win)
 			return;
 	}
 
