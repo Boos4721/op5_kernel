3680762320c21f14e283d08b0d07d0991e10e419
Sultan Alsawaf <sultan@kerneltoast.com>
2020-02-28 12:22:08 -0800

    mm: vmpressure: Make local functions and global variables static
    These functions/symbols are not used externally, so make them static.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -239,7 +239,8 @@ static void vmpressure_work_fn(struct work_struct *work)
 	} while ((vmpr = vmpressure_parent(vmpr)));
 }
 
-static unsigned long calculate_vmpressure_win(void)
+static void vmpressure_memcg(gfp_t gfp, struct mem_cgroup *memcg,
+			     unsigned long scanned, unsigned long reclaimed)
 {
 	long x;
 
@@ -292,7 +293,30 @@ static void vmpressure_memcg(gfp_t gfp, struct mem_cgroup *memcg, bool critical,
 	schedule_work(&vmpr->work);
 }
 
-static void vmpressure_global(gfp_t gfp, unsigned long scanned, bool critical,
+static void calculate_vmpressure_win(void)
+{
+	long x;
+
+	x = global_page_state(NR_FILE_PAGES) -
+			global_page_state(NR_SHMEM) -
+			total_swapcache_pages() +
+			global_page_state(NR_FREE_PAGES);
+	if (x < 1)
+		x = 1;
+	/*
+	 * For low (free + cached), vmpressure window should be
+	 * small, and high for higher values of (free + cached).
+	 * But it should not be linear as well. This ensures
+	 * timely vmpressure notifications when system is under
+	 * memory pressure, and optimal number of events when
+	 * cached is high. The sqaure root function is empirically
+	 * found to serve the purpose.
+	 */
+	x = int_sqrt(x);
+	vmpressure_win = x;
+}
+
+static void vmpressure_global(gfp_t gfp, unsigned long scanned,
 			      unsigned long reclaimed)
 {
 	struct vmpressure *vmpr = &global_vmpressure;
--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 7 */
+/* This file is auto generated, version 9 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#7 SMP PREEMPT Thu Mar 19 09:26:11 CST 2020"
+#define UTS_VERSION "#9 SMP PREEMPT Fri Mar 20 10:06:11 CST 2020"
 #define LINUX_COMPILE_BY "boos"
 #define LINUX_COMPILE_HOST "hentai"
 #define LINUX_COMPILER "Android (6284175 based on r377782c) clang version 10.0.5 (https://android.googlesource.com/toolchain/llvm-project 0cb1afdb060d6619a9837ab6866cf4adf2336416)"
